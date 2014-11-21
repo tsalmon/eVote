153c77fb8bc3c939da9cc1045e8a6f152ae01164
Vladislav Fitc <Vladislav.Fitz@gmail.com>
2014-11-21 01:46:21 +0100

    La derniere version du code

---
+++ src/main/src/Elections/ElectionsManager.scala
@@ -0,0 +1,21 @@
+package Elections
+
+abstract class ElectionsManager[ElectionsType <: Elections, VotingPaperType <: VotingPaper](val notificationName: String, val electors:Set[Voting]) extends Observer {
+  NotificationCenter.addObserver(this, notificationName)
+
+  protected var votes = List[VotingPaperType]()
+
+  def createVotingPaper(voting: Voting): VotingPaperType
+
+  def notify(notification:Notification) = {
+    val votingPaper = notification.internalObject
+    notification.internalObject match{
+      case votingPaper : Some[VotingPaperType] => votes =  votingPaper.get::votes
+      case _ => ()
+    }
+  }
+
+  def createElections:ElectionsType
+
+  def printVotes = println(votes)
+}
