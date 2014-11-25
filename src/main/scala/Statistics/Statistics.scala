a1c593776db07ab1ba54f5a885db9424a1313839
NGUYEN Thi Quynh Nga <quynhnga.k59spt@gmail.com>
2014-11-25 15:20:48 +0100

    statistic v1

--- src/main/scala/Statistics/Statistics.scala
+++ src/main/scala/Statistics/Statistics.scala
@@ -4,63 +4,56 @@ import Elections.{District, Electable, VotingPaper, Elections}
 
 
 abstract class Statistics {
- type VotingPapers = List[VotingPaper]
- var candidateResults : List[CandidateResult]
- var districtResults: List[DistrictResult]
- 
-  def set_Stat_CandidateResult(election: Elections) = {
-	 for (candidate <- election.candidates){
-//	   (CandidateResult(candidate))::candidateResults
-	 }
-  }
- 
-  def set_Stat_DistrictResult(election: Elections) =  {
-	 for(district <- election.districts){
-	   
-	 }
-  }
-  
+   var nbElectionVotes : Int
+   var candidateResults : List[CandidateResult]
+   var districtResults: List[DistrictResult]
+   
+   def addCandidateResult(CR: CandidateResult) = CR::candidateResults
+   def addDistrictResult(DR: DistrictResult)= DR::districtResults
+   def removeCandidateResult(CR: CandidateResult)= {
+     candidateResults.filter(_ != CR);
+   }
+   def removeDistrictResult(DR: DistrictResult)= {
+     districtResults.filter(_ != DR);
+   }
 }
 
 abstract class CandidateResult(val _candidate:Electable){
     var candidate = _candidate;
     var nbVotes : Int;
     var result : Double;
-    def caculateCandidatResult(votingPapers: List[VotingPaper])= {
+    def caculateCandidateResult(votingPapers: List[VotingPaper])= {
       for (voting <- votingPapers){
-	      if (voting.toString.contains(_candidate)) {
-	        this.nbVotes= this.nbVotes + 1;
-	      }
+        if (voting.toString.contains(_candidate)) {
+          this.nbVotes= this.nbVotes + 1;
+        }
       }
       this.result = votingPapers.length / this.nbVotes *100 
     }
 }
 
 abstract class DistrictResult(val _district:District){
-
-	var resultDistrict : List[(Electable,Int, Double)]; // List((electable, (votes, percent)))
-	var votesDistrict : Int; // for counter the votes in this District
-	votesDistrict = 0;
-	
-	// setting the format of the result in this District
-	def formResultDistrict(election: Elections) = {
-	  for (candidate <- election.candidates){
-	    resultDistrict = (candidate,0,0.00)::resultDistrict
-	  }
-	}
-	
-    def calculateDistrictResult(votingPapers: List[VotingPaper]) = {
-      
-      for (voting <- votingPapers){
-        if (voting.district == _district){
-          votesDistrict = votesDistrict+1
-          for ((electable,votes, percent) <- resultDistrict){
-            if(voting.toString.contains(electable)){
-//              (electable,votes+1,votes+1/votesDistrict)
-	        }
-          }
-          
+  
+  var nbDistrictVotes: Int
+  var districtVotingPapers: List[VotingPaper]
+  // List result in this district : List((electable, votes, percent))
+  var resultDistrict : List[CandidateResult]; 
+  
+  def add_districtVotingPaper(votingPaper: VotingPaper)= votingPaper::districtVotingPapers
+  
+  def remove_districtVotingPaper(votingPaper: VotingPaper) = districtVotingPapers.filter(_ != votingPaper)
+  
+  // filter votingPaper of District in list_VotingPaper of Election
+    def set_districtVotingPapers(votingPapers:List[VotingPaper])={
+      for(votingPaper <- votingPapers){
+        if (votingPaper.district == _district){
+          add_districtVotingPaper(votingPaper)
         }
       }
-    }
+    } 
+    
+  def add_resultDistrict(CR: CandidateResult) = CR::resultDistrict
+  
+  def remove_resultDistrict(CR: CandidateResult) = resultDistrict.filter(_ != CR)
+   
 }
