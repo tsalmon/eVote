1c43f03d3f292e28833d420cc4bc40d73b5dfb44
NGUYEN Thi Quynh Nga <quynhnga.k59spt@gmail.com>
2014-11-25 13:37:44 +0100

    module statistic

--- src/main/scala/Statistics/Statistics.scala
+++ src/main/scala/Statistics/Statistics.scala
@@ -1,15 +1,66 @@
 package Statistics
 
-import Elections.{District, Electable, VotingPaper}
+import Elections.{District, Electable, VotingPaper, Elections}
+
 
 abstract class Statistics {
-  var votingPapers:List[VotingPaper]
+ type VotingPapers = List[VotingPaper]
+ var candidateResults : List[CandidateResult]
+ var districtResults: List[DistrictResult]
+ 
+  def set_Stat_CandidateResult(election: Elections) = {
+	 for (candidate <- election.candidates){
+//	   (CandidateResult(candidate))::candidateResults
+	 }
+  }
+ 
+  def set_Stat_DistrictResult(election: Elections) =  {
+	 for(district <- election.districts){
+	   
+	 }
+  }
+  
 }
 
-abstract class CandidateResult(val candidate:Electable){
-
+abstract class CandidateResult(val _candidate:Electable){
+    var candidate = _candidate;
+    var nbVotes : Int;
+    var result : Double;
+    def caculateCandidatResult(votingPapers: List[VotingPaper])= {
+      for (voting <- votingPapers){
+	      if (voting.toString.contains(_candidate)) {
+	        this.nbVotes= this.nbVotes + 1;
+	      }
+      }
+      this.result = votingPapers.length / this.nbVotes *100 
+    }
 }
 
-abstract class DistrictResult(val district:District){
+abstract class DistrictResult(val _district:District){
 
+	var resultDistrict : List[(Electable,Int, Double)]; // List((electable, (votes, percent)))
+	var votesDistrict : Int; // for counter the votes in this District
+	votesDistrict = 0;
+	
+	// setting the format of the result in this District
+	def formResultDistrict(election: Elections) = {
+	  for (candidate <- election.candidates){
+	    resultDistrict = (candidate,0,0.00)::resultDistrict
+	  }
+	}
+	
+    def calculateDistrictResult(votingPapers: List[VotingPaper]) = {
+      
+      for (voting <- votingPapers){
+        if (voting.district == _district){
+          votesDistrict = votesDistrict+1
+          for ((electable,votes, percent) <- resultDistrict){
+            if(voting.toString.contains(electable)){
+//              (electable,votes+1,votes+1/votesDistrict)
+	        }
+          }
+          
+        }
+      }
+    }
 }
