a0a4719703f85646b7e8f8552491d879f8899a3b
tsalmon <th_s@hotmail.fr>
2014-11-30 12:51:23 +0100

    tests init

---
+++ src/test/scala/Condorcet.scala
@@ -0,0 +1,12 @@
+
+package Condorcet
+
+import org.scalatest._
+import Elections._
+import ElectionsResultProcessing.ElectionsResultProcessing
+
+class ElectionsTest extends FlatSpec{
+  "Condorcet" should "return test" {
+    
+  }
+}
