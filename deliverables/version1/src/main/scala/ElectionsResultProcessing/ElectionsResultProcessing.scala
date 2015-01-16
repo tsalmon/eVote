9d8c83189427f05a93c6d60d61898d10692ee20a
NGUYEN Thi Quynh Nga <quynhnga.k59spt@gmail.com>
2015-01-16 22:48:13 +0100

    fini partie 1

--- deliverables/version1/src/main/scala/ElectionsResultProcessing/ElectionsResultProcessing.scala
+++ deliverables/version1/src/main/scala/ElectionsResultProcessing/ElectionsResultProcessing.scala
@@ -17,9 +17,21 @@ abstract class ElectionsResultProcessingMajority[ElectionsType <: Elections]
   def calculate(listResultRate:List[(Any,Double)]) = listResultRate.sortWith((x,y)=>x._2>y._2).head
 }
 
-
 abstract class ElectionsResultProcessingProportional[ElectionsType <: Elections]
   extends ElectionsResultProcessing[ElectionsType] {
   def calculate(listResultRate:List[(Any,Double)], nbSeat: Int) =
     listResultRate.map((x) => (x._1,(x._2*nbSeat/100).round))
+}
+
+abstract class ElectionsResultProcessingMixed[ElectionsType <: Elections]
+  extends ElectionsResultProcessing[ElectionsType] {
+  def calculate(listResultRate:List[(Any,Double)], nbSeat: Int) = {
+    var listResultMixed:List[(Any, Long)] = Nil
+    //apply method proportional on the half of seat
+    listResultMixed = listResultRate.map((x) => (x._1,(x._2*nbSeat/200).round))
+    //apply method majority on the rest
+    listResultMixed = listResultMixed.sortWith((x,y)=>x._2>y._2)
+
+    (listResultMixed.head._1, listResultMixed.head._2 + nbSeat/2)::listResultMixed.drop(1)
+  }
 }
