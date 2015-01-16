4415bee66544c84aa9d1fa2c244f68c70f317d0f
NGUYEN Thi Quynh Nga <quynhnga.k59spt@gmail.com>
2015-01-17 00:00:07 +0100

    ajoute les commantaire

--- deliverables/version1/src/main/scala/ElectionsResultProcessing/ElectionsResultProcessing.scala
+++ deliverables/version1/src/main/scala/ElectionsResultProcessing/ElectionsResultProcessing.scala
@@ -3,26 +3,34 @@ package ElectionsResultProcessing
 import Elections.{Electable, Elections}
 
 abstract class ElectionsResultProcessing[ElectionsType <: Elections]{
+  //calculate the rate of each candiate
   def calculateRate(listResult:List[(Any,Int)],nbVoting:Int):List[(Any, Double)]=
     listResult match {
       case (candidate, score)::listRest =>
         (candidate, (score.toDouble*100/nbVoting.toDouble).toDouble)::calculateRate(listRest, nbVoting)
       case _ => Nil
     }
+
+  //calculate the result, the user will definer this fonction
   def calculateResult(elections: ElectionsType): elections.VoteResult
 }
 
+//processing for system majority
 abstract class ElectionsResultProcessingMajority[ElectionsType <: Elections]
   extends ElectionsResultProcessing[ElectionsType] {
+  //sort the list and access the first element in list
   def calculate(listResultRate:List[(Any,Double)]) = listResultRate.sortWith((x,y)=>x._2>y._2).head
 }
 
+//processing for system proportional
 abstract class ElectionsResultProcessingProportional[ElectionsType <: Elections]
   extends ElectionsResultProcessing[ElectionsType] {
+  //calculate the amount of seat for each candidate
   def calculate(listResultRate:List[(Any,Double)], nbSeat: Int) =
     listResultRate.map((x) => (x._1,(x._2*nbSeat/100).round))
 }
 
+//processing for system mixed
 abstract class ElectionsResultProcessingMixed[ElectionsType <: Elections]
   extends ElectionsResultProcessing[ElectionsType] {
   def calculate(listResultRate:List[(Any,Double)], nbSeat: Int) = {
