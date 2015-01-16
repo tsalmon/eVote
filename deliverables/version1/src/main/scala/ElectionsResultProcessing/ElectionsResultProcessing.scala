c5f951245468e0876841c16f93dd5cb316e6b0d1
NGUYEN Thi Quynh Nga <quynhnga.k59spt@gmail.com>
2015-01-16 21:37:31 +0100

    ajoute majorityMethode

--- deliverables/version1/src/main/scala/ElectionsResultProcessing/ElectionsResultProcessing.scala
+++ deliverables/version1/src/main/scala/ElectionsResultProcessing/ElectionsResultProcessing.scala
@@ -3,5 +3,16 @@ package ElectionsResultProcessing
 import Elections.{Electable, Elections}
 
 abstract class ElectionsResultProcessing[ElectionsType <: Elections]{
+  def calculateRate(listResult:List[(Any,Int)],nbVoting:Int):List[(Any, Double)]=
+    listResult match {
+      case (candidate, score)::listRest =>
+        (candidate, (score.toDouble*100/nbVoting.toDouble).toDouble)::calculateRate(listRest, nbVoting)
+      case _ => Nil
+    }
   def calculateResult(elections: ElectionsType): elections.VoteResult
+}
+
+abstract class ElectionsResultProcessingMajority[ElectionsType <: Elections]
+  extends ElectionsResultProcessing[ElectionsType] {
+  override def calculateMajority(listResultRate:List[(Any,Double)]) = listResultRate.sortWith((x,y)=>x._2>y._2).head
 }
